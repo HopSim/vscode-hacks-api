3aed147e90af22d81a14d8914501743dd302c33a
logansimonsen <logansimonsen@hotmail.com>
2018-12-19 16:01:41 -0700

    added /random endpoint for getting random tips

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3001
+var _ = require('underscore');
+
+const array = ['Tip1', 'Tip2', 'Tip3', 'Tip4']
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+//http://localhost:3001/random returns random array element
+app.get('/random', (req, res) => res.send(_.sample(array)))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
