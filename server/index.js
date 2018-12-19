d6795ba4b709fa4116ce19be8b9aa24fcb4cc914
logansimonsen <logansimonsen@hotmail.com>
2018-12-19 16:54:35 -0700

    added search endpoint, which isn't entirely scoped out yet, but could be used for search functionality in the future

--- server/index.js
+++ server/index.js
@@ -10,4 +10,7 @@ app.get('/', (req, res) => res.send('Hello World!'))
 //http://localhost:3001/random returns random array element
 app.get('/random', (req, res) => res.send(_.sample(array)))
 
+//http://localhost:3001/search could possibily pass in a variable from the user to filter results
+app.get('/search', (req, res) => res.send(_.filter(array, function (element) { return element.includes("3") })))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
