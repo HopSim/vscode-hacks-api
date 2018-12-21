1f1a266b7454c0982b7cb83dce1cc0589ed3cd47
logansimonsen <logansimonsen@hotmail.com>
2018-12-20 21:02:13 -0700

    changed search endpoint so that it can accept query parameters

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ app.get('/', (req, res) => res.send('Hello World!'))
 //http://localhost:3001/random returns random array element
 app.get('/random', (req, res) => res.send(_.sample(array)))
 
-//http://localhost:3001/search could possibily pass in a variable from the user to filter results
-app.get('/search', (req, res) => res.send(_.filter(array, function (element) { return element.includes("3") })))
+//http://localhost:3001/search?value=3 example OR http://localhost:3001/search?value=Tip
+app.get('/search', (req, res) => res.send(_.filter(array, function (element) {
+    // console.log(req.query.value)
+    return element.includes(req.query.value)
+})))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`listening on port ${port}!`))
