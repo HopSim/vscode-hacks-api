64b0ed507f2da0e9c56ef0923caa07cf5ac89589
logansimonsen <logansimonsen@hotmail.com>
2018-12-21 23:03:51 -0700

    added db folder/file. removed buggy search endpoint. added success status code/messages

---
+++ db/db.js
@@ -0,0 +1,23 @@
+module.exports.tips = [
+    {
+        id: 1,
+        title: "Multi cursor selection",
+        description: "To add cursors at arbitrary positions, select a position with your mouse and use Alt+Click (Option+click on macOS)."
+    },
+    {
+        id: 2,
+        title: "Command Palette",
+        description: "Access all available commands based on your current context. Keyboard Shortcut: ⇧⌘P"
+    },
+    {
+        id: 3,
+        title: "Quick Open",
+        description: "Quickly open files. Keyboard Shortcut: ⌘P"
+    }
+];
+
+
+// Quick Open#
+// Quickly open files.
+
+// Keyboard Shortcut: ⌘P
